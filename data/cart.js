2e215e63ccca2bd01809c3a5b83aceb2fc935108
Nandwa <nandwajapheth7@gmail.com>
2024-10-13 13:51:49 +0300

    used functions to organise the codes in fresh.js file

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addProduceToCart(produceId) {
+  let existingItem;
+
+  cart.forEach((cartProduct) => {
+    if (produceId === cartProduct.produceId) {
+      existingItem = cartProduct;
+    }
+  });
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      produceId: produceId,
+      quantity: 1,
+    });
+  }
+}
